63d995c54edb8f6e8d40fea1789bda757f59b26d
Alexander Vassilev <av@mega.co.nz>
2017-02-17 00:04:24 +0200

    Fixed cmake toolchain variables to be stored in cache, as otherwise they don't have the correct value at first run

--- env-stm32.sh
+++ env-stm32.sh
@@ -21,6 +21,7 @@ STM32_CMAKE_TOOLCHAIN="$STM32_SYSROOT/stm32-toolchain.cmake"
 # Create cmake toolchain file
 echo -e "\n\
 #Generated file, do not edit by hand
+cmake_policy(VERSION 3.4)\n\
 set(CMAKE_SYSTEM_NAME Generic)\n\
 set(CMAKE_SYSTEM_PROCESSOR arm)\n\
 set(CMAKE_SYSROOT \"$STM32_SYSROOT\")\n\
@@ -30,8 +31,8 @@ set(CMAKE_CXX_COMPILER arm-none-eabi-g++)\n\
 set(CMAKE_ASM_COMPILER arm-none-eabi-as)\n\
 set(CMAKE_LINKER arm-none-eabi-ld)\n\
 \n\
-set(CMAKE_C_FLAGS \"\${CMAKE_C_FLAGS} -fno-common -mcpu=cortex-m3 -mthumb\")
-set(CMAKE_CXX_FLAGS \"\${CMAKE_C_FLAGS}\")
+set(CMAKE_C_FLAGS \"\${CMAKE_C_FLAGS} -fno-common -mcpu=cortex-m3 -mthumb -nostartfiles\" CACHE STRING \"\")
+set(CMAKE_CXX_FLAGS  \"\${CMAKE_CXX_FLAGS} \${CMAKE_C_FLAGS}\" CACHE STRING \"\")
 \n\
 set(optLinkScript \"$STM32_LINK_SCRIPT\" CACHE PATH \"Linker script\")\n\
 set(optChipFamily STM32F10X_MD CACHE STRING \"Chip family for platform headers\")\n\
@@ -39,12 +40,14 @@ set_property(CACHE optChipFamily PROPERTY STRINGS STM32F10X_LD STM32F10X_LD_VL\n
     STM32F10X_MD STM32F10X_MD_VL STM32F10X_HD STM32F10X_HD_VL STM32F10X_XL STM32F10X_CL)\n\
 \n\
 add_definitions(-D\${optChipFamily})\n\
-set(CMAKE_EXE_LINKER_FLAGS \"-nostartfiles -T\${optLinkScript}\")\n\
+set(CMAKE_EXE_LINKER_FLAGS \"-nostartfiles -T\${optLinkScript}\" CACHE STRING \"\")\n\
 \n\
 set(CMAKE_FIND_ROOT_PATH \"$STM32_SYSROOT\")\n\
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)\n\
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)\n\
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)\n\
+\n\
+set(CMAKE_BUILD_TYPE Debug CACHE STRING \"Build type\")\n\
 \n\
 " > "$STM32_CMAKE_TOOLCHAIN"
 
