3b06ea5f5a1d92dad360ff0c3d77ac230a502a17
Alexander Vassilev <av@mega.co.nz>
2017-02-19 13:55:57 +0200

    Some conveniences in env script

--- env-stm32.sh
+++ env-stm32.sh
@@ -18,23 +18,33 @@ function xcmake
 {
     cmake $CMAKE_XCOMPILE_ARGS "$@"
 }
+export -f xcmake
 
 function ocmd
 {
     $owndir/ocmd.sh "$@"
 }
+export -f ocmd
 
 function flash
 {
     $owndir/flash.sh "$@"
 }
+export -f flash
+
+export PS1="[\u@\033[0;32m\033[3mstm32\033[0m \W]\$"
 
 # Convenience alias
-alias gdb="arm-none-eabi-gdb"
+alias gdb=arm-none-eabi-gdb
+alias objcopy=arm-none-eabi-objcopy
+alias gcc=arm-none-eabi-gcc
+alias g++=arm-none-eabi-g++
+alias as=arm-eabi-as
+alias nm=arm-eabi-nm
 
 echo -e "Your environment has been set up for STM32 cross-compilation.\n\n\
 SYSROOT=$STM32_SYSROOT\n
 Use the 'xcmake' command instead of 'cmake' in order to configure project for\n\
 cross-compilation\n\
 Use the 'flash' command to flash chip, see flash --help for details\n\
-Use 'ocmd <command[;command[...]]>' to send any command to OpenOCD.\n"
+Use 'ocmd <commands>' to send any command to OpenOCD.\n"
