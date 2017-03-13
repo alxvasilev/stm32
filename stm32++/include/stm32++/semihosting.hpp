89addd320e0524f67b8e61fd83617b248bed74b8
Alexander Vassilev <av@mega.co.nz>
2017-03-13 14:16:31 +0200

    Make semihosting code stanalone

---
+++ stm32++/include/stm32++/semihosting.hpp
@@ -0,0 +1,34 @@
+#if !defined(SEMIHOSTING_HPP) && !defined(NOT_EMBEDDED)
+#define SEMIHOSTING_HPP
+
+namespace shost
+{
+/** @brief Semihosting opcodes */
+enum: uint8_t {
+    SYS_WRITE = 0x05,
+    SYS_READ = 0x06,
+    SYS_READC = 0x07,
+    SYS_TIME = 0x11
+};
+
+size_t bkpt(size_t cmd, size_t arg1);
+
+void fputs(const char* str, size_t len, int fd, void* userp);
+void write(void* buf, size_t bufsize, int fd=1);
+
+/** @brief Reads a char from semihosting stdin and returns it. If the stdin
+ * is not connected, -1 is returned (EOF).
+ */
+static inline int getchar() { return bkpt(SYS_READC, 0); }
+
+/** @brief Read data from semihosting stdin.
+ * @return Number of bytes read
+ */
+size_t read(void* buf, size_t bufsize, int fd=0);
+
+/** @brief Returns the unix time from the host system */
+static inline uint32_t time() { return bkpt(SYS_TIME, 0); }
+
+};
+
+#endif
---
+++ stm32++/semihosting.cpp
@@ -0,0 +1,52 @@
+#include <stm32++/snprint.h>
+#include <stm32++/semihosting.hpp>
+
+#ifdef NOT_EMBEDDED
+#error NOT_EMBEDDED is set:\n\
+Semihosting can be used only when compiling for embedeed target
+#endif
+
+static_assert(sizeof(size_t) == sizeof(void*));
+
+namespace shost
+{
+
+/** Single-argument wrapper for the BKPT instruction. Note that some commands
+ * use a second argument in r2
+ */
+size_t bkpt(size_t cmd, size_t arg1)
+{
+    size_t ret;
+    asm volatile(
+       "mov r0, %[cmd];"
+       "mov r1, %[arg1];"
+       "bkpt #0xAB;"
+       "mov %[ret], r0;"
+       : [ret] "=r" (ret) //out
+       : [cmd] "r" (cmd), [arg1] "r" (arg1) //in
+       : "r0", "r1", "memory" //clobber
+    );
+    return ret;
+}
+
+void write(const void* buf, size_t bufsize, int fd)
+{
+    size_t msg[3] = { (size_t)fd, (size_t)buf, bufsize };
+    bkpt(SYS_WRITE, (size_t)msg);
+}
+
+void fputs(const char* str, size_t len, int fd, void* userp)
+{
+    write(str, len, fd);
+}
+
+size_t read(void* buf, size_t bufsize, int fd)
+{
+  size_t msg[3] = { (size_t)fd, (size_t)buf, bufsize };
+  //SYS_READ returns the number of bytes remaining in the  buffer,
+  //i.e. bufsize - actual_bytes_read
+  size_t ret = bkpt(SYS_READ, (size_t)msg);
+  return bufsize-ret;
+}
+
+}
