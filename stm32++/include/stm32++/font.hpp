bf96ecd5c3e7c7ec13ee019013c5e77427ddb736
Alexander Vassilev <av@mega.co.nz>
2020-12-18 17:57:12 +0200

    Font stuff improvements

--- stm32++/include/stm32++/font.hpp
+++ stm32++/include/stm32++/font.hpp
@@ -9,17 +9,19 @@ struct Font
     const uint8_t count;
     const uint8_t* widths;
     const uint8_t* data;
-    Font(uint8_t aWidth, uint8_t aHeight, uint8_t aCount, const uint8_t* aWidths, const void* aData)
-    :width(aWidth), height(aHeight), count(aCount), widths(aWidths), data((uint8_t*)aData)
+    const uint8_t codeOffset;
+    Font(uint8_t aWidth, uint8_t aHeight, uint8_t aCount, const uint8_t* aWidths, const void* aData, uint8_t aCodeOfs=32)
+    :width(aWidth), height(aHeight), count(aCount), widths(aWidths), data((uint8_t*)aData),
+        codeOffset(aCodeOfs)
     {}
     bool isMono() const { return widths == nullptr; }
     const uint8_t* getCharData(uint8_t code) const
     {
         if (!widths) {
-            if (code < 32) {
+            if (code < codeOffset) {
                 return nullptr;
             }
-            code -= 32;
+            code -= codeOffset;
             if (code >= count) {
                 return nullptr;
             }
